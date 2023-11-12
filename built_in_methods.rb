d4ad5e687f144298b135d4c8eb09a278d367593b
Hoa Dam <hoangocdam@gmail.com>
2023-11-11 22:43:02 -0800

    Practice built_in_methods

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -10,47 +10,90 @@
 
 
 "Hello World".include?("Hello")
-
+# The include method is called on the string object "Hello World" in order to check if the word "Hello" is included in the String.
+# The return value is True
 
 "Hello World".end_with?("Hello")
-
+# The end_with method is called on the tring object "Hello World" in order to check if the string is end with "Hello"
+# The return value is false
 
 "Hello World".end_with?("rld")
-
+# The end_with method is called on the tring object "Hello World" in order to check if the string is end with "rld"
+# The return value is true
 
 12.even?
+# The even method is called on the integer 12 in order to check if the integer 12 is an even number.
+# The return value is true
 
 
 18.next
+# The next medthod is called on the integer 18 in order to return the next successor of the integer 18.
+# The return value is 19
 
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
 # Declare 2 variables assigned to string objects.
-# Call a different built-in Ruby method on each of your variables. 
+# Call a different built-in Ruby method on each of your variables.
 # https://ruby-doc.org/core-3.1.0/String.html
 # Include comments above each method call explaining the impact and return value of that method.
 
 # EXAMPLE
-# The start_with? method is called on the first_name variable, which stores the string object "Jeff". 
+# The start_with? method is called on the first_name variable, which stores the string object "Jeff".
 # The start_with? method returns true if the data in the first_name variable starts with the argument passed in.
 # In this example, the return value is true, because "Jeff" does start with "J".
 # The puts command prints the return value of the start_with? method (true) to the console.
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
+pet_name = "spike"
+  puts pet_name.capitalize!
+# The capitalize method is called on the pet_name variable, which stores the string object "spike".
+# The capitalize! method returns upcases the initial character.
+# In this example, the puts command prints the return value "Spike"
+
+country_name = "Vietnam"
+  puts country_name.size
+# The size method is called on the country_name variable, which stores the string object "Vietnam".
+# The size method returns the count of characters.
+# In this example, the puts command prints the return value 7 that is total characters of the string "Vietnam"
+
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
-# Call a different built-in Ruby method on each of your variables. 
+# Call a different built-in Ruby method on each of your variables.
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
+number1 = 484
+  puts Math.sqrt(number1)
+# The square root method is called on the number1 variable, which stores the integer 484.
+# The square root method returns the integer square root of the given value.
+# In this example, the puts command prints the return value 22 that is the square root of 484.
+
+number2 = 72
+  puts number2.to_f
+# The to_f method is called on the number2 variable, which stores the integer 72.
+# The to_f method returns the integer converted to a float.
+# In this example, the puts command prints the return value 72.0 that is the float value of the integer 72.
 
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
-# Call a different built-in Ruby method on each of your variables. 
+# Call a different built-in Ruby method on each of your variables.
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+array1 = [2,4,6]
+  puts array1.delete_if {|a| a < 2}
+# The delete_if method is called on the array1 variable, which stores 3 elements 2,4, and 6.
+# The delete_if method will delete the element that is less than 2.
+# In this example, the puts command prints the value of array1 = [2,4,6] because all the elements are bigger than 2.
+
+
+array2 = [76,117,3010]
+  puts array2.first
+# The first method is called on the array2 variable, which stores 3 elements 76, 117 and 3010.
+# The first method returns the first element in the array.
+# In this example, the puts command prints the return value 76 that is the first element of the given array2.
--- final_practice.rb
+++ final_practice.rb
@@ -1,27 +1,44 @@
 # Defining Our Own Methods
-# For each exercise below, write the method according to the requirements. 
-# Call each method at least twice and store the return value in a variable. 
+# For each exercise below, write the method according to the requirements.
+# Call each method at least twice and store the return value in a variable.
 # Use the puts or print command to see the return value in the console.
 
 
-# 1: Write a method named greeting that returns a string with a general greeting. 
+# 1: Write a method named greeting that returns a string with a general greeting.
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
-
+def greeting
+  "Hello World"
+end
+puts greeting
+# The return value of the greeting method is the string "Hello World"
+# The greeting method does not take any arguments
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
-
-
+def custom_greeting (first_name, last_name)
+  "Hello, #{first_name} #{last_name}!"
+end
+puts custom_greeting("Hoa", "Dam")
+# The return value of the custom_greeting method is a string that includes the provided name in the greeting.
+# There is 2 arguments which is "first_name", "last_name"
+# The data type of the argument is a string
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
+def custom_greeting (first_name, middle_name, last_name)
+  "Hello, #{first_name} #{middle_name} #{last_name}!"
+end
+puts custom_greeting("Hoa", "Ngoc", "Dam")
+# The return value of the method is a string "Hello, Hoa Ngoc Dam!"
+# There are 3 arguments
+# The data type of the argument is a string
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
@@ -30,6 +47,13 @@
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
+def square(number)
+  number * number
+end
+puts square(5)
+# The return value of the method is 25 that is the square of 5.
+# There is 1 element.
+# Data type is an integer.
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
@@ -46,3 +70,15 @@ check_stock(0, "Cheese");
 
 check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
+
+def check_stock(number,name)
+  if number <= 3
+    puts "#{name} - running low"
+  elsif number == 0
+    puts "#{name} - OUT of stock"
+  else
+    puts "#{name} is stocked"
+  end
+end
+check_stock(0, "Cheese")
